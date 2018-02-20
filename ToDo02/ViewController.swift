8b6e6933c0b296cb2cdb5a14d00666b93c70c46d
釜野由里佳 <yrkmn19@gmail.com>
2018-02-20 21:15:09 +0900

    TableView作り直し

--- ToDo02/ViewController.swift
+++ ToDo02/ViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource{
+    
+    //Storyboardで扱うTableViewを宣言
+    @IBOutlet var table: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //テーブルビューのデータソースメソッドはViewControllerクラスに書くよ、という設定
+        table.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //セルの数を設定
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    //ID付きのセルを取得して、セル付属のtextLabelに「テスト」と表示させてみる　//★ここを変えるのかな！
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        
+        cell?.textLabel?.text = "テスト"
+        
+        return cell!
+    }
 
 }
-
