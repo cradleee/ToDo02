f030eca731a7b9ea91ff46621cd29494c3e5bc6e
釜野由里佳 <yrkmn19@gmail.com>
2018-02-21 18:21:19 +0900

    作り直し/保存機能がうまくいかない

--- ToDo02/MemoViewController.swift
+++ ToDo02/MemoViewController.swift
@@ -12,20 +12,54 @@ class MemoViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet var titleTextField: UITextField!
     @IBOutlet var contentTextView: UITextView!
+    
+    var saveDate: UserDefaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        titleTextField.text = saveDate.object(forKey: "title") as? String
+        contentTextView.text = saveDate.object(forKey: "content") as? String
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+    //#007メモ帳　p.２５
     @IBAction func saveMemo() {
+        //UserDefaultsに書き込み
+        saveDate.set(titleTextField.text, forKey: "title")
+        saveDate.set(contentTextView.text, forKey: "content")
         
+        //alertを出す
+        let alert: UIAlertController = UIAlertController(title: "保存", message:"メモの保存が完了しました。",
+                                                         preferredStyle: .alert)
+        
+        //OKボタン
+        alert.addAction(
+                    UIAlertAction(
+                        title: "OK",
+                        style: .default,
+                        handler: { action in
+                            //ボタンが押されましいたときの動作
+                            //#007 メモ帳P.31
+                            self.navigationController?.popViewController(animated: true)
+                            print("OKボタンが押されました！")
+                        }
+                    )
+                )
+        present(alert, animated: true, completion: nil)
     }
+    
+    //画面遷移
+    //override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    //var MemoViewController:MemoViewController = segue.destinationViewController as
+    //MemoViewController.param = self.paramText.text
+    //}
+    
+    //func shouldPerformSegue(withIdentifier: String?, sender: AnyObject?)
+    //return true
     
     /*
     // MARK: - Navigation
